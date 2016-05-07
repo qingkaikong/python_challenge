95d4fb682653be8a7935726905def54769faf765
Qingkai Kong <qingkai.kong@gmail.com>
2016-05-06 21:49:03 -0700

    my solution, somehow the o is not showing

---
+++ level1/sol_1.py
@@ -0,0 +1,11 @@
+from string import maketrans   # Required to call maketrans function.
+
+intab = "abcdefghijklmnopqrstuvwxyz"
+outtab = "cdefghijklmnopqrstuvwxyzab"
+trantab = maketrans(intab, outtab)
+
+str = 'g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr\'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
+str2 = 'map'
+print str.translate(trantab, 'xm')
+
+print str2.translate(trantab, 'xm')
